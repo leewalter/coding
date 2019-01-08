a55c0c848b836c07f0cd8e0ce3c8cd427bad0c5b
Walter Lee <quietwalter@gmail.com>
2019-01-07 22:41:55 -0800

    added regex for Jumbo loans only
    jumboRegex = re.compile('Jumbo', re.IGNORECASE )

--- python/wf-rates1.py
+++ python/wf-rates1.py
@@ -1,7 +1,11 @@
 # very simple code to scrape Wells Fargo Mortgage Rates from html table
+# ref - https://www.youtube.com/watch?v=F1kZ39SvuGE
 # ref - https://github.com/engineer-man/youtube/tree/master/042
+
+
 import requests
 from bs4 import BeautifulSoup
+import re
 
 # get the data
 data = requests.get('https://www.wellsfargo.com/mortgage/rates/')
@@ -12,20 +16,28 @@ soup = BeautifulSoup(data.text, 'html.parser')
 # XPath //*[@id="PurchaseRatesTable"]
 mortgagetable = soup.find('table', { 'id': 'PurchaseRatesTable' })
 tbody = mortgagetable.find('tbody')
+
+#print title line
 print ("%-30s %10s %10s" % ("productName", "intRate", "AprRate"))
+
+# print only if Jumbo loans
+jumboRegex = re.compile('Jumbo', re.IGNORECASE )
 
 for tr in tbody.find_all('tr'):
     try:
         productName = tr.find_all('th')[0].find_all('a')[0].text.strip()
-        intRate = tr.find_all('td')[0].text.strip()
-        AprRate = tr.find_all('td')[1].text.strip()
-        # print(productName,"\t\t", intRate,"\t", AprRate)
-        print ("%-30s %10s %10s" % (productName, intRate, AprRate))
+        if jumboRegex.search(productName): 
+            intRate = tr.find_all('td')[0].text.strip()
+            AprRate = tr.find_all('td')[1].text.strip()
+            print ("%-30s %10s %10s" % (productName, intRate, AprRate))
+        else:
+            continue
     except IndexError:
         pass
 
 '''
-output: 
+output 1 when print ALL out : 
+
 (base) D:\github\walter-repo\python\web-scape\>python wf-rates1.py
 productName                       intRate    AprRate
 30-Year Fixed Rate                 4.375%      4.48%
@@ -38,4 +50,13 @@ productName                       intRate    AprRate
 15-Year Fixed-Rate Jumbo            3.75%     3.827%
 7/1 ARM Jumbo                      3.375%     4.415%
 10/1 ARM Jumbo                      3.75%     4.347%
+
+output 2 when regex to Jumbo loans only because it is expensive in SF : ) 
+
+productName                       intRate    AprRate
+30-Year Fixed-Rate Jumbo           4.125%     4.148%
+15-Year Fixed-Rate Jumbo            3.75%     3.845%
+7/1 ARM Jumbo                        3.5%     4.443%
+10/1 ARM Jumbo                      3.75%     4.357%
+
 '''
