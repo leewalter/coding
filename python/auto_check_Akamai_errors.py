a4db1b6d1d42cae3933223f16e82486304819bfe
Walter Lee <quietwalter@gmail.com>
2019-01-12 21:18:46 -0800

    Update auto_check_Akamai_errors.py
    add Twilio SMS

--- python/auto_check_Akamai_errors.py
+++ python/auto_check_Akamai_errors.py
@@ -2,6 +2,26 @@
 import os, sys 
 import time, datetime
 import json
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/console; should use env. file below if PROD, this is a quick test in 10min below
+account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+auth_token = '6xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+client = Client(account_sid, auth_token)
+
+def twilio_send_sms(EdgeErrorPercent):
+
+    if ( EdgeErrorPercent > 10 ):  # if error % > 10%, then send SMS for Alert ! 
+        smsbody="Alert ! Site www.abc.com Edge Error % is high at " + str(EdgeErrorPercent) + "%!"
+        #print (smsbody)
+   
+        message = client.messages.create(
+                            body=smsbody,
+                            from_='+17xxxxxxxxxx',
+                            to='+14xxxxxxxxxxxxx'
+                        )
+        print(message.sid)
+
 
 def current_time():
     ts = time.time()
@@ -10,8 +30,8 @@ def current_time():
 
 def check_akamai_errors():
     # below will call Akamai API to get back current errors stats for a CPcode config
-    cmd="http  --auth-type edgegrid -a default: --timeout=120  :/diagnostic-tools/v2/cpcodes/123456789/estats > Akamai_error.txt" 
-    returned_value = os.system(cmd)  # returns the exit code in unix
+    cmd="http  --auth-type edgegrid -a default: --timeout=120  :/diagnostic-tools/v2/cpcodes/1234567890/estats > Akamai_error.txt" 
+    returned_value = os.system(cmd)  # returns the exit code in unix, can do more error checking
     # print('returned value:', returned_value)
 
 while True:
@@ -26,6 +46,8 @@ while True:
     # Print the errors %
     print ("edgeFailurePercentage_value: %10s" %(edgeFailurePercentage_value))
     print ("originFailurePercentage_value: %10s" %(originFailurePercentage_value))
+    #edgeFailurePercentage_value=40 # fake a 40% error to test SMS
+    twilio_send_sms(edgeFailurePercentage_value) 
     time.sleep(120)
 
 '''
