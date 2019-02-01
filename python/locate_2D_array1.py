4fc2e2918fbb9184f2a30431c85bd5db60cae8df
Walter Lee <quietwalter@gmail.com>
2019-01-31 19:53:29 -0800

    add functions and use list comprehension

--- python/locate_2D_array1.py
+++ python/locate_2D_array1.py
@@ -1,15 +1,70 @@
 # python to locate 1 in a 2D array
 
+def check_zero(array1):
+
+    print("array index zeros at:")
+    for i in range(len(array1)):
+        index = [k for k, v in enumerate(array1[i]) if v == 0]
+
+        print(i, index)
+
+    return(i,index)
+
 array1 = [
   [1, 1, 0, 0],
   [0, 0, 1, 1],
   [0, 1, 0, 1]
 ]
 
+array2 = [
+  [1, 1, 0, 0, 0],
+  [0, 0, 1, 1, 0],
+  [0, 0, 1, 0, 1]
+]
+
+check_zero(array1)
+
+check_zero(array2)
+
+'''
+array index zeros at:
+0 [2, 3]
+1 [0, 1]
+2 [0, 2]
+array index zeros at:
+0 [2, 3, 4]
+1 [0, 1, 4]
+2 [0, 1, 3]
+'''
+
+
+
+
+array1 = [
+  [1, 1, 0, 0],
+  [0, 0, 1, 1],
+  [0, 1, 0, 1]
+]
+
+for i in range(len(array1)):
+    index = [ k for k,v in enumerate(array1[i]) if v ==0 ]
+    print(i, index)
+    
+'''
+outputs 
+0 [2, 3]
+1 [0, 1]
+2 [0, 2]
+
+'''
+
+
+''' very primitive below, 
 for i in range(0,3):
     for j in range(0,4):
         if (array1[i][j] == 1):
             print(i,j)
+'''
 
 '''
 # https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.nonzero.html
