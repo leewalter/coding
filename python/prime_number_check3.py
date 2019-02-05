6f2581122a16526b992a0675f89959adf1ce7822
Walter Lee <quietwalter@gmail.com>
2019-02-05 12:44:53 -0800

    only check if odd number and 2

--- python/prime_number_check3.py
+++ python/prime_number_check3.py
@@ -17,62 +17,39 @@ def check_prime(n):
 
 prime_list = []
 for i in range(-1,50):
-    if (check_prime(i)):
-        prime_list.append(i)
+    if i % 2 == 1 or i ==2 :
+        if (check_prime(i)):
+            prime_list.append(i)
 
 print(prime_list)
 
 '''
 -1 is not a prime no
-0 is not a prime no
 1 is not a prime no
 2 is a prime no
 3 is a prime no !
-4 is not a prime no because divisible by 2
 5 is a prime no !
-6 is not a prime no because divisible by 2
 7 is a prime no !
-8 is not a prime no because divisible by 2
 9 is not a prime no because divisible by 3
-10 is not a prime no because divisible by 2
 11 is a prime no !
-12 is not a prime no because divisible by 2
 13 is a prime no !
-14 is not a prime no because divisible by 2
 15 is not a prime no because divisible by 3
-16 is not a prime no because divisible by 2
 17 is a prime no !
-18 is not a prime no because divisible by 2
 19 is a prime no !
-20 is not a prime no because divisible by 2
 21 is not a prime no because divisible by 3
-22 is not a prime no because divisible by 2
 23 is a prime no !
-24 is not a prime no because divisible by 2
 25 is not a prime no because divisible by 5
-26 is not a prime no because divisible by 2
 27 is not a prime no because divisible by 3
-28 is not a prime no because divisible by 2
 29 is a prime no !
-30 is not a prime no because divisible by 2
 31 is a prime no !
-32 is not a prime no because divisible by 2
 33 is not a prime no because divisible by 3
-34 is not a prime no because divisible by 2
 35 is not a prime no because divisible by 5
-36 is not a prime no because divisible by 2
 37 is a prime no !
-38 is not a prime no because divisible by 2
 39 is not a prime no because divisible by 3
-40 is not a prime no because divisible by 2
 41 is a prime no !
-42 is not a prime no because divisible by 2
 43 is a prime no !
-44 is not a prime no because divisible by 2
 45 is not a prime no because divisible by 3
-46 is not a prime no because divisible by 2
 47 is a prime no !
-48 is not a prime no because divisible by 2
 49 is not a prime no because divisible by 7
 [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
 
