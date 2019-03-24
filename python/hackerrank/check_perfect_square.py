2cdc68086d399d0a4e45b34ca241682daea3800d
Walter Lee <quietwalter@gmail.com>
2019-03-24 09:36:28 -0700

    how to check if a perfect square of a range of integers
    how to check if a perfect square of a range of integers

---
+++ python/hackerrank/check_perfect_square.py
@@ -0,0 +1,38 @@
+'''
+https://www.hackerrank.com/challenges/sherlock-and-squares/submissions/code/102921740
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the squares function below.
+def squares(a, b):
+    count = 0
+    for i in range(a,b+1):
+        i_sqrt = math.sqrt(i)
+        if  i_sqrt.is_integer() :  # if integer, then a perfect square 
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        ab = input().split()
+
+        a = int(ab[0])
+
+        b = int(ab[1])
+
+        result = squares(a, b)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
